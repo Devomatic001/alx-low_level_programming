e94cc45a99d9807f6eca99d9e706cd1d274fb8bb
Devomatic001 <stephensundaytemitayo@gmail.com>
2022-10-13 15:30:19 -0700

    ooo

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
 /**
- * main - function puts
+ *  main -Entry point
  *
- * Return: 0
-*/
-
-int main (void)
+ *  Return: Always 0 (Success)
+ */
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
